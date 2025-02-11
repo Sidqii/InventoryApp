6883652691996540375f5be57e21ed2bcc673047
Sidqii <SidqiMhmmd412@gmail.com>
2025-02-12 00:43:31 +0700

    upgrade

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:pusdatin_end/pages/page_login.dart';
-import 'package:pusdatin_end/providers/providers_login.dart';
-import 'package:pusdatin_end/providers/providers_pegawai.dart';
-import 'package:pusdatin_end/providers/providers_dataBarang.dart';
-import 'package:pusdatin_end/providers/providers_peminjaman.dart';
-import 'package:pusdatin_end/providers/providers_pemeliharaan.dart';
-import 'package:pusdatin_end/providers/providers_persetujuan.dart';
 
 void main() {
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => AuthService()),
-        ChangeNotifierProvider(create: (context) => ProviderPegawai()),
-        ChangeNotifierProvider(create: (context) => ProviderDatabarang()),
-        ChangeNotifierProvider(create: (context) => ProviderPeminjaman()),
-        ChangeNotifierProvider(create: (context) => ProviderPemeliharaan()),
-        ChangeNotifierProvider(create: (context) => ProviderPersetujuan()),
-        // -> Provider lain.
-      ],
-      child: MyApp(),
-    ),
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/login',
-      routes: {
-        '/login':(context) => LoginPage(),
-      },
-      title: 'Pusdatin App',
       theme: ThemeData(
-        primaryColor: Colors.transparent,
-        scaffoldBackgroundColor: Colors.transparent,
+        useMaterial3: true,
       ),
-      home: LoginPage(),
     );
   }
 }
